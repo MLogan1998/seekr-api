955f352adeaa3a7c3bf2b174d91228b40082b07b
Matt Logan <mlogan5212@gmail.com>
2021-03-06 21:12:53 -0600

    Profile Form WIP

--- seekr/urls.py
+++ seekr/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from rest_framework import routers
 from django.urls import path
 from django.conf.urls import include
-from seekrapi.views import register_user, login_user, SeekerProfileViewSet
+from seekrapi.views import register_user, login_user, SeekerProfileViewSet, LanguagesViewSet
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'seekrs', SeekerProfileViewSet, 'seekr')
+router.register(r'languages', LanguagesViewSet, 'language')
 
 
 urlpatterns = [
