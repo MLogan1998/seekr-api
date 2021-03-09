39c8580ef5e2288cf596fa9e980a8e568a8c3e20
Matt Logan <mlogan5212@gmail.com>
2021-03-09 17:55:51 -0600

    Employer Profile WIP

--- seekr/urls.py
+++ seekr/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 from rest_framework import routers
 from django.urls import path
 from django.conf.urls import include
-from seekrapi.views import register_user, login_user, SeekerProfileViewSet, LanguagesViewSet, SeekerProfileViewSet
+from seekrapi.views import register_user, login_user, SeekerProfileViewSet, LanguagesViewSet, SeekerProfileViewSet, EmployerProfileViewSet
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'seekrs', SeekerProfileViewSet, 'seekr')
 router.register(r'languages', LanguagesViewSet, 'language')
 router.register(r'profile', SeekerProfileViewSet, 'profile')
+router.register(r'employerprofile', EmployerProfileViewSet, 'profile')
 
 
 urlpatterns = [
--- seekrapi/models/employer_profile.py
+++ seekrapi/models/employer_profile.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.conf import settings
 from django.db.models import CASCADE
 
 class EmployerProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, default=3, on_delete=CASCADE)
+    user = models.ForeignKey("User", related_name="employerProfiles", related_query_name="EmployerProfile", on_delete=CASCADE, default=1)
     profile_img = models.CharField(max_length=200)  
