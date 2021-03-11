19d40cbd2a24c48d52de69022d9c53a669d3be58
Matt Logan <mlogan5212@gmail.com>
2021-03-10 20:24:13 -0600

    employer forms complete

--- seekr/urls.py
+++ seekr/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework import routers
 from django.urls import path
 from django.conf.urls import include
-from seekrapi.views import register_user, login_user, SeekerProfileViewSet, LanguagesViewSet, SeekerProfileViewSet, EmployerProfileViewSet, UserViewSet
+from seekrapi.views import register_user, login_user, SeekerProfileViewSet, LanguagesViewSet, SeekerProfileViewSet, EmployerProfileViewSet, UserViewSet, CompanyProfileViewSet, JobPostingViewSet
 
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -11,6 +11,8 @@ router.register(r'seekrs', SeekerProfileViewSet, 'seekr')
 router.register(r'languages', LanguagesViewSet, 'language')
 router.register(r'profile', SeekerProfileViewSet, 'profile')
 router.register(r'employerprofile', EmployerProfileViewSet, 'profile')
+router.register(r'company', CompanyProfileViewSet, 'company')
+router.register(r'joblisting', JobPostingViewSet, 'company')
 router.register(r'user', UserViewSet, 'user')
 
 
--- seekrapi/models/job_posting.py
+++ seekrapi/models/job_posting.py
@@ -4,5 +4,7 @@ from django.db.models import CASCADE
 class JobPosting(models.Model):
     employer = models.ForeignKey("EmployerProfile", on_delete=CASCADE)
     job_description = models.TextField()
-    salary = models.IntegerField()
+    salary = models.CharField(max_length=250)
     benefits = models.BooleanField()
+    requirements = models.TextField()
+    job_title = models.CharField(max_length=250)
